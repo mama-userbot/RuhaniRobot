1cd130de3a8db37a00bee3e71cd73c9e1e5f8eb2
mama-userbot <77100101+mama-userbot@users.noreply.github.com>
2021-01-15 18:51:52 +0530

    Update afk.py

--- SaitamaRobot/modules/afk.py
+++ SaitamaRobot/modules/afk.py
@@ -57,9 +57,9 @@ def no_longer_afk(update: Update, context: CallbackContext):
         firstname = update.effective_user.first_name
         try:
             options = [
-                '{} is here!', '{} is back!', '{} is now in the chat!',
+                '{} darling is here!', '{} is here!', '{} is back!', '{} is now in the chat!',
                 '{} is awake!', '{} is back online!', '{} is finally here!',
-                'Welcome back! {}', 'Where is {}?\nIn the chat!'
+                'Welcome back! {}', 'Where is {}?\nIn the chat!', '{} none missed you!'
             ]
             chosen_option = random.choice(options)
             update.effective_message.reply_text(chosen_option.format(firstname))
@@ -135,8 +135,8 @@ def check_afk(update, context, user_id, fst_name, userc_id):
 
 
 __help__ = """
- • `/afk <reason>`*:* mark yourself as AFK(away from keyboard).
- • `brb <reason>`*:* same as the afk command - but not a command.
+ - `/afk <reason>`*:* mark yourself as AFK(away from keyboard).
+ - `brb <reason>`*:* same as the afk command - but not a command.
 When marked as AFK, any mentions will be replied to with a message to say you're not available!
 """
 
