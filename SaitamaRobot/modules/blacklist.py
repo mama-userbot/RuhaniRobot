c1f123592b3c9deeb941ea229fdc6bdfefc44007
mama-userbot <77100101+mama-userbot@users.noreply.github.com>
2021-01-16 00:23:44 +0530

    Update blacklist.py

--- SaitamaRobot/modules/blacklist.py
+++ SaitamaRobot/modules/blacklist.py
@@ -453,16 +453,15 @@ __mod_name__ = "Blacklists"
 
 __help__ = """
 
-Blacklists are used to stop certain triggers from being said in a group. Any time the trigger is mentioned, the message will immediately be deleted. A good combo is sometimes to pair this up with warn filters!
+It is used to stop certain triggers from being said in a group. Any time the trigger is mentioned, the message will immediately be deleted. you can also set penalty with it.
+*NOTE*: It will not affect group admins.
 
-*NOTE*: Blacklists do not affect group admins.
+ - `/blacklist`*:* View the current blacklisted words.
 
- • `/blacklist`*:* View the current blacklisted words.
-
-Admin only:
- • `/addblacklist <triggers>`*:* Add a trigger to the blacklist. Each line is considered one trigger, so using different lines will allow you to add multiple triggers.
- • `/unblacklist <triggers>`*:* Remove triggers from the blacklist. Same newline logic applies here, so you can remove multiple triggers at once.
- • `/blacklistmode <off/del/warn/ban/kick/mute/tban/tmute>`*:* Action to perform when someone sends blacklisted words.
+➩ Admin only:
+ - `/addblacklist <triggers>`*:* Add a trigger to the blacklist. Each line is considered one trigger, so using different lines will allow you to add multiple triggers.
+ - `/unblacklist <triggers>`*:* Remove triggers from the blacklist. Same newline logic applies here, so you can remove multiple triggers at once.
+ - `/blacklistmode <off/del/warn/ban/kick/mute/tban/tmute>`*:* Action to perform when someone sends blacklisted words.
 
 """
 BLACKLIST_HANDLER = DisableAbleCommandHandler(
