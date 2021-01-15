e00efdd9c2eb2d923789b8a5207abf7b8a2735a5
mama-userbot <77100101+mama-userbot@users.noreply.github.com>
2021-01-16 00:05:59 +0530

    Update approval.py

--- SaitamaRobot/modules/approval.py
+++ SaitamaRobot/modules/approval.py
@@ -196,12 +196,9 @@ def unapproveall_btn(update: Update, context: CallbackContext):
 
 
 __help__ = """
-Sometimes, you might trust a user not to send unwanted content.
-Maybe not enough to make them admin, but you might be ok with locks, blacklists, and antiflood not applying to them.
+On approving a user then locks, blacklists, and antiflood qill not applying to them.
 
-That's what approvals are for - approve of trustworthy users to allow them to send 
-
-*Admin commands:*
+➩ *Admin commands:*
 - `/approval`*:* Check a user's approval status in this chat.
 - `/approve`*:* Approve of a user. Locks, blacklists, and antiflood won't apply to them anymore.
 - `/unapprove`*:* Unapprove of a user. They will now be subject to locks, blacklists, and antiflood again.
