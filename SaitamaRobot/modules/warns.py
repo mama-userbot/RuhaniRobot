5a10180c4591c503745d82c5b9b4231e612f32ca
mama-userbot <77100101+mama-userbot@users.noreply.github.com>
2021-01-16 18:41:20 +0530

    Update warns.py

--- SaitamaRobot/modules/warns.py
+++ SaitamaRobot/modules/warns.py
@@ -119,7 +119,7 @@ def warn(user: User,
         ]])
 
         reply = (
-            f"<code>❕</code><b>Warn Event</b>\n"
+            f"<code>❕</code><b>\\Warned_User//</b>\n"
             f"<code> </code><b>•  User:</b> {mention_html(user.id, user.first_name)}\n"
             f"<code> </code><b>•  Count:</b> {num_warns}/{limit}")
         if reason:
@@ -491,17 +491,17 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
- • `/warns <userhandle>`*:* get a user's number, and reason, of warns.
- • `/warnlist`*:* list of all current warning filters
+ - `/warns <userhandle>`*:* get a user's number, and reason, of warns.
+ - `/warnlist`*:* list of all current warning filters
 
-*Admins only:*
- • `/warn <userhandle>`*:* warn a user. After 3 warns, the user will be banned from the group. Can also be used as a reply.
- • `/resetwarn <userhandle>`*:* reset the warns for a user. Can also be used as a reply.
- • `/addwarn <keyword> <reply message>`*:* set a warning filter on a certain keyword. If you want your keyword to \
+➩ *Admins only:*
+ - `/warn <userhandle>`*:* warn a user. After 3 warns, the user will be banned from the group. Can also be used as a reply.
+ - `/resetwarn <userhandle>`*:* reset the warns for a user. Can also be used as a reply.
+ - `/addwarn <keyword> <reply message>`*:* set a warning filter on a certain keyword. If you want your keyword to \
 be a sentence, encompass it with quotes, as such: `/addwarn "very angry" This is an angry user`. 
- • `/nowarn <keyword>`*:* stop a warning filter
- • `/warnlimit <num>`*:* set the warning limit
- • `/strongwarn <on/yes/off/no>`*:* If set to on, exceeding the warn limit will result in a ban. Else, will just punch.
+ - `/nowarn <keyword>`*:* stop a warning filter
+ - `/warnlimit <num>`*:* set the warning limit
+ - `/strongwarn <on/yes/off/no>`*:* If set to on, exceeding the warn limit will result in a ban. Else, will just punch.
 """
 
 __mod_name__ = "🔸 Warns"
