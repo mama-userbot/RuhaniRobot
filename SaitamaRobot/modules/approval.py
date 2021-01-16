4ad48ca876b2edd8abaeb69211552ec859cf7179
mama-userbot <77100101+mama-userbot@users.noreply.github.com>
2021-01-16 16:09:21 +0530

    Update approval.py

--- SaitamaRobot/modules/approval.py
+++ SaitamaRobot/modules/approval.py
@@ -220,6 +220,6 @@ dispatcher.add_handler(APPROVAL)
 dispatcher.add_handler(UNAPPROVEALL)
 dispatcher.add_handler(UNAPPROVEALL_BTN)
 
-__mod_name__ = "🔸 Approval"
+__mod_name__ = "🔸 Approval "
 __command_list__ = ["approve", "unapprove", "approved", "approval"]
 __handlers__ = [APPROVE, DISAPPROVE, APPROVED, APPROVAL]
